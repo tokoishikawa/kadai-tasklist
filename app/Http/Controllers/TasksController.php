f749486ed503eb4603525d24440adaf6b5796d52
EC2 Default User <ec2-user@ip-172-31-9-87.us-east-2.compute.internal>
2018-05-28 03:35:53 +0000

    add column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -45,10 +45,12 @@ class TasksController extends Controller
      public function store(Request $request)
     {
          $this->validate($request, [
+             'title' => 'required|max:191', 
             'content' => 'required|max:191',
         ]);
         
         $task = new Task;
+         $task->title = $request->title;  
         $task->content = $request->content;
         $task->save();
 
@@ -66,10 +68,17 @@ class TasksController extends Controller
 
      public function update(Request $request, $id)
     {
+        $this->validate($request, [
+        'title' => 'required|max:191', 
+         'content' => 'required|max:191',
+    ]);
+
         $task = Task::find($id);
+        $task->title = $request->title;   
         $task->content = $request->content;
         $task->save();
-
+    
+       
         return redirect('/');
     }
     
--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -13,4 +13,15 @@
 
     {!! Form::close() !!}
 
+
+     @if (count($errors) > 0)
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    @endif
+    
 @endsection
+
+
